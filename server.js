074052399f16167a2c1d523091a01bed346e19a7
RISHIKUMAR206 <rishikumarjha005@gmail.com>
2026-02-20 03:27:02 -0300

    feat: verified backend logs and finalized docker setup

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Backend is running perfectly!</h1><p>Rishi DevOps Project - Port 3000</p>');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log('Server is live at http://localhost:3000');
+});
